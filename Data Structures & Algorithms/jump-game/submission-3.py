4d0662be37557eca0197ba531a5baae9621554df
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-08 14:36:00 -0700

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        failed = set()
+
+        def dfs(index):
+            if index in failed:
+                return False
+            
+            if index >= len(nums) - 1:
+                return True
+            
+            for jump in range(nums[index], 0, -1):
+                if dfs(index + jump):
+                    return True
+                
+            failed.add(index)
+            return False
+        return dfs(0)
