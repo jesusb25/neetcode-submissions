c13ad3fd195a3d923708bf45b74c049b05d800bd
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-09 22:27:54 -0700

    Add: valid-tree - submission-11

---
+++ Data Structures & Algorithms/valid-tree/submission-11.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # all nodes reachable, no cycles, and n-1 edges
+        if len(edges) != n - 1:
+            return False
+        
+        adj = defaultdict(list) # node: neighbors
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        # marked visited after all neighbors
+        # keep previous to avoid traveling in circle without prev
+        visited = set()
+        def hasCycle(node, parent):
+            if node in visited:
+                return True
+            
+            visited.add(node)
+
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if hasCycle(nei, node):
+                    return True
+            return False
+
+        # since nodes shoudl be connected can start anywhere
+        # if start anywhere and 
+        return not hasCycle(0, -1) and len(visited) == n
