7901d29d607110fc0964ba5bf856872ef258bbcb
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-10 18:17:28 -0700

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # adj list using neighoring words
+        adj = defaultdict(set) # word; neighbors
+        def checkNei(word1, word2):
+            diffs = 0
+            for i in range(len(word1)):
+                if word1[i] != word2[i]:
+                    diffs += 1
+                
+                if diffs > 1:
+                    return False
+            return diffs == 1
+
+        for word in wordList + [beginWord]:
+            for nei in wordList + [beginWord]:
+                if checkNei(word, nei) and word != nei:
+                    adj[nei].add(word)
+                    adj[word].add(nei)
+
+        
+        q = deque([[beginWord, 1]])
+        seen = set()
+        while q:
+            curr, changes = q.popleft()
+            if curr == endWord:
+                return changes
+
+            if curr in seen:
+                continue
+            
+            seen.add(curr)
+            for nei in adj[curr]:
+                q.append([nei, changes + 1])
+
+
+
+
+        return 0
+
+
+
