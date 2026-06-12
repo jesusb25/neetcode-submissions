28f334e76cbda8b7726f01831cb687cbf3bd19c4
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-12 11:16:34 -0700

    Add: accounts-merge - submission-9

---
+++ Data Structures & Algorithms/accounts-merge/submission-9.py
@@ -0,0 +1,58 @@
+class UnionFind:
+    def __init__(self, n):
+        # parents [num] parents of each self
+        self.par = [i for i in range(n)]
+        # rank order to keep parents
+        self.rank = [1] * n
+
+    # find parent
+    def find(self, x):
+        # find parents iteratively
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+
+    def union(self, x1, x2):
+        p1, p2 = self.find(x1), self.find(x2)
+        # if same parents already in one group
+        if p1 == p2:
+            return False
+        
+        # share parents of higher rank, increase rank
+        # union
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p1] += self.rank[p2]
+        return True
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+
+        # email -> index of acc
+        emailToAcc = {}
+        for i, a in enumerate(accounts):
+            for e in a[1:]:
+                # if email already mapped, union
+                if e in emailToAcc:
+                    uf.union(i, emailToAcc[e])
+                else:
+                    emailToAcc[e] = i
+
+        # index of acc -> list of emails
+        emailGroup = defaultdict(list)
+        for e, i in emailToAcc.items():
+            # parent
+            leader = uf.find(i)
+            # set account parent to gropu
+            emailGroup[leader].append(e)
+
+        res = []
+        for i, emails in emailGroup.items():
+            name = accounts[i][0]
+            res.append([name] + sorted(emailGroup[i]))
+        return res
