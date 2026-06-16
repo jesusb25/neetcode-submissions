a313019debc40014b98c77edc40bc8f8f6161d8f
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-16 09:24:03 -0700

    Add: time-based-key-value-store - submission-7

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-7.py
@@ -0,0 +1,38 @@
+class TimeMap:
+
+    def __init__(self):
+        # time key value store
+        self.hashmap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.hashmap:
+            self.hashmap[key] = []
+        self.hashmap[key].append([timestamp, value])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        # binary search keep left time always move right
+        if key not in self.hashmap:
+            return ""
+        entries = self.hashmap[key]
+        if entries[0][0] > timestamp:
+            return ""
+        
+        left = 0
+        right = len(entries) - 1
+        res = -1
+
+        # [1, 2, 3]
+        #         l      r
+        while left <= right:
+            mid = (left + right) // 2
+            if entries[mid][0] > timestamp:
+                right = mid - 1
+            else:
+                res = mid
+                left = mid + 1
+
+        
+        return entries[res][1]
+
+
