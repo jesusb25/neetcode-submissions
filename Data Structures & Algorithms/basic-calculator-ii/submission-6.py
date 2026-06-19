6862df00b180b53024571ecac505f48963b59c1a
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-19 11:36:16 -0400

    Add: basic-calculator-ii - submission-6

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        op = "+"
+        num = 0
+        stack = []
+        s = s.replace(" ", "")
+
+        for i, char in enumerate(s):
+            # build num
+            if char.isdigit():
+                num = num * 10 + int(char)
+            
+            # if last num or op
+            if i == len(s) - 1 or not char.isdigit():
+                if op == "+":
+                    stack.append(num)
+                elif op == "*":
+                    stack.append(stack.pop() * num)
+                elif op == "/":
+                    stack.append(int(stack.pop() / num))
+                else:
+                    stack.append(-num)
+                op = char
+                num = 0
+        print(stack)
+        return sum(stack)
+                
