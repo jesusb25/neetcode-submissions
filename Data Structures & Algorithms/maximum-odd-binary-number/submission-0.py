6493696d5349e2b1758624441b2056849c605620
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-20 06:23:29 -0400

    Add: maximum-odd-binary-number - submission-0

---
+++ Data Structures & Algorithms/maximum-odd-binary-number/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones = 0
+        for c in s:
+            if c == "1":
+                ones += 1
+
+        return (ones - 1) * "1" + "0" * (len(s) - ones)  + "1"
