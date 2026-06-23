24d96151533af544bc97bf1cd7fac6dfeafa521f
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-23 13:58:17 -0400

    Add: online-stock-span - submission-3

---
+++ Data Structures & Algorithms/online-stock-span/submission-3.py
@@ -0,0 +1,27 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.prices = [] # always decrease
+        self.days = 0
+        
+
+    def next(self, price: int) -> int:
+        self.days += 1
+
+        # remove same prices days to avoid returning distances between two same price days
+        while self.prices and self.prices[-1][0] <= price:
+            self.prices.pop()
+            
+        self.prices.append([price, self.days])
+        
+        if len(self.prices) > 1:
+            return self.days - self.prices[-2][1]
+        return self.days
+        
+        
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
