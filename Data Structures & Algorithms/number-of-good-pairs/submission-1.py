757b0da94c48c3fcadbfd6583ed5c3bb6c136c7b
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-06-23 23:05:23 -0400

    Add: number-of-good-pairs - submission-1

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        indices = defaultdict(list)
+        for i, num in enumerate(nums):
+            indices[num].append(i)
+        
+        res = 0
+        for lst in indices.values():
+            n = len(lst)
+            res += n * (n - 1) // 2
+        return res
