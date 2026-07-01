be8dfb610e91b78a13ed5e75c26fd3728b28d2ff
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-07-01 07:03:15 -0500

    Add: longest-increasing-subsequence - submission-10

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dfs(i, prev):
+            if i == len(nums) or (i, prev) in memo:
+                return memo.get((i, prev), 0)
+
+            res = dfs(i + 1, prev)
+            if prev < nums[i]:
+                res = max(res, 1 + dfs(i + 1, nums[i]))
+            
+            memo[(i, prev)] = res
+            return res
+
+        return dfs(0, -float('inf'))
+            
+            
