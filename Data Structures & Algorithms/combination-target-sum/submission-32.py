1ca78b61f8b64e57a89a754e51052e53e0d73537
jesusb25 <104487170+jesusb25@users.noreply.github.com>
2026-07-06 10:51:55 -0500

    Add: combination-target-sum - submission-32

---
+++ Data Structures & Algorithms/combination-target-sum/submission-32.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        path = []
+        memo = set() # index, total
+
+        def dfs(index, total):
+            if total == target:
+                res.append(path[::])
+                return 
+
+            if total > target or index == len(nums):
+                return 
+            
+            # include
+            path.append(nums[index])
+            dfs(index, total + nums[index])
+            path.pop()
+
+            # exclude
+            dfs(index + 1, total)
+
+            
+        dfs(0, 0)
+        return res
